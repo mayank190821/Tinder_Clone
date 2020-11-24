deb8f8c583a223ccd3a9c8d4b4009fac9283376b
Mayank Bhugra <mayank.123.2000@gmail.com>
2020-11-24 16:39:42 +0530

    adding server , linking with react, create header

--- tinder-clone/src/App.js
+++ tinder-clone/src/App.js
@@ -1,16 +1,17 @@
 import React from "react";
 import './App.css';
 import Header from "./header"
-
+import TinderCards from "./TinderCards";
 function App() {
   return (
     <div className="App">
 
       {/* Header */}
-      
       <Header  />
 
       {/* tinder card */}
+      <TinderCards />
+
       {/* SwipeButton */}
     </div>
   );
---
+++ tinder-clone/src/TinderCards.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './TinderCards.css';
+
+function TinderCards() {
+    return (
+        <div className="tinderCards">
+            <h1>Tinder TinderCards</h1>
+        </div>
+    )
+}
+
+export default TinderCards
