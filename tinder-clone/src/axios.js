b96a2aadc356b8a45548e259cbfb3ec9f577c810
Mayank Bhugra <mayank.123.2000@gmail.com>
2020-11-26 16:42:53 +0530

    update axios

--- tinder-clone/src/axios.js
+++ tinder-clone/src/axios.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
 const instance = axios.create({
-    baseURL:"https://tinder-1908.herokuapp.com/"
-})
+    baseURL:"https://tinder-1908.herokuapp.com",
+});
 export default instance;
